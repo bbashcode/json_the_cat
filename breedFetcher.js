4d3d264e16f470954ed6a91f8c7427d70e9468e8
bbashcode <binbash.code@gmail.com>
2022-01-23 20:40:50 +0000

    checking the string payload conversion to JSON object using JSON.parse()

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,9 +1,4 @@
 const request = require('request');
-/**
- * api endpoint: https://api.thecatapi.com/v1/breeds?=%27siberian%27 or  https://api.thecatapi.com/v1/breeds?=%27siberian%27
- *  
- * 
- */ 
 
 let userInput = process.argv.slice(2);
 let url = 'https://api.thecatapi.com/v1/breeds/search?q=Siberian';
@@ -11,9 +6,14 @@ let url = 'https://api.thecatapi.com/v1/breeds/search?q=Siberian';
 request((url), (error, response, body) => {
 
     if(error){
-      console.log(error);
+      console.log({error});
     }
-    console.log(response);
-    console.log(body);
+    console.log(`body: `, body);
 
+    console.log(`type of body`, typeof body);
+    
+    console.log('--------------------------------------');
+    const data = JSON.parse(body);
+    console.log(`data`, data);
+    console.log(`type of data`, typeof data);
   });
