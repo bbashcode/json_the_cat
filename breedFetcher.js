1f0418b6f0df2bfe91f6b2481e9a654a0b43ebd1
bbashcode <binbash.code@gmail.com>
2022-01-23 21:00:08 +0000

    cats as a service breed details fetcher has been implemented:

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,24 +2,27 @@ const request = require('request');
 
 let userInput = process.argv.slice(2);
 
+// allow the user to specify the breed name using command-line arguments.
 let url = 'https://api.thecatapi.com/v1/breeds/search?q=' + userInput;
 
 request((url), (error, response, body) => {
 
+    // Handle request errors and print the error details to the screen.  
     if(error){
-      console.log({error});
+      console.log('Here is the error: ', error);
     }
-    console.log(`body: `, body);
 
-    console.log(`type of body`, typeof body);
     
-    console.log('--------------------------------------');
+    // use JSON.parse to convert the JSON string into an actual object.
     const data = JSON.parse(body);
-    console.log(`data`, data);
-    console.log(`type of data`, typeof data);
 
+    // Edge Case: Breed Not Found
+    if (data.length === 0 || data[0] === 'undefined') {
+      console.log(`Sorry, the requested breed: '${userInput}' cannot be found!`);
+    } else { // access the first entry in the data array and print out the description for the user.
+      console.log(`Breed Description: `, data[0].description);
+      console.log(`Breed Details: `, data[0]);
 
-    console.log('+++++++++++++++++++++++++++++++++++++++');
-    console.log(`first elemenet of data`, data[0]);
-
+    }
+    
   });
