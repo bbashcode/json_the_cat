f66490d22e99a328a47ff674df2e6e02ffc9cea6
bbashcode <binbash.code@gmail.com>
2022-01-24 00:19:53 +0000

    refactored cat as a service

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,28 +1,27 @@
 const request = require('request');
 
-let userInput = process.argv.slice(2);
 
-// allow the user to specify the breed name using command-line arguments.
-let url = 'https://api.thecatapi.com/v1/breeds/search?q=' + userInput;
+let apiParam = 'https://api.thecatapi.com/v1/breeds/search?q=';
 
-request((url), (error, response, body) => {
 
-    // Handle request errors and print the error details to the screen.  
-    if(error){
-      console.log('Here is the error: ', error);
-    }
+const fetchBreedDescription = (breedName, callback) => {
 
-    
-    // use JSON.parse to convert the JSON string into an actual object.
+  let endPoint = apiParam + breedName;
+
+  request(endPoint, (error, response, body) => {
+
     const data = JSON.parse(body);
 
-    // Edge Case: Breed Not Found
-    if (data.length === 0 || data[0] === 'undefined') {
-      console.log(`Sorry, the requested breed: '${userInput}' cannot be found!`);
-    } else { // access the first entry in the data array and print out the description for the user.
-      console.log(`Breed Description: `, data[0].description);
-      console.log(`Breed Details: `, data[0]);
-
+    if (error) {
+      return callback(error, null);
+    } else {
+      if (data.length === 0 || data[0] === 'undefined') {
+        return callback(null, 'Sorry, the requested cat cannot be found!');
+      } else {
+        console.log(data[0].description);
+      }
     }
-    
   });
+};
+//Export the function using module
+module.exports = {fetchBreedDescription};
